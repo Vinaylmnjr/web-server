680719305405dc6c76a85d92f4dd8c0a339f94b2
Vinaysastha <vvvsgvinaysastha@gmail.com>
2018-02-08 13:18:37 +0530

    Initialize repo

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+// app.get('/',function(req ,res){
+// 	res.send('Welcome to my World');
+// });
+var middleware = {
+		requireAuthentication: function(req , res, next){
+			console.log('Private route hit!');
+			next();
+		},
+		logger: function(req , res, next){
+			console.log('Request: '+ new Date().toString() + ' ' +req.method +''+req.originalUrl);
+			next();
+		}
+	
+};
+
+app.use(middleware.logger);
+
+//app.use(middleware.requireAuthentication);
+
+app.get('/about', function(req,res){
+	res.send('About Us');
+})
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(PORT,function(){
+	console.log('Express Server is Started on PORT: '+PORT);
+});
